75b861c67e1504e7dca66e8f7a328e846f6c93ff
Krixxx <32840063+Krixxx@users.noreply.github.com>
2024-03-23 10:48:06 +0200

    add removal option for old backup files

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 BACKUP_DIR=~/backups
 VOLUME_NAME=ng-server-settings_postgres_data
+MAX_DAYS=30
 
 # Create backup directory if it doesn't exist
 mkdir -p $BACKUP_DIR
@@ -11,3 +12,6 @@ mkdir -p $BACKUP_DIR
 # Run backup container
 docker run --rm -v $VOLUME_NAME:/volume -v $BACKUP_DIR:/backup ubuntu \
     tar czvf /backup/postgres_backup_$TIMESTAMP.tar.gz -C /volume .
+
+# Remove backups older than 30 days
+find $BACKUP_DIR -name "postgres_backup_*" -type f -mtime +$MAX_DAYS -exec rm {} \;
